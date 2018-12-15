2f6e08e4e4d9c3f7122840282506f07fb357f95f
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-12-14 19:38:21 -0500

    api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 module.exports = function(app){
-    app.get('/api/friends',function(req,res){
-       res.send("in friends API"); 
+    app.get('/api/classes',function(req,res){
+       res.send("displaying api data"); 
+    });
+
+    app.post('/api/classes',function(req,res){
+        console.log(req);
     });
 };
