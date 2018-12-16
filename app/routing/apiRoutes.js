c7f0ad7d920b3324487f662e1a0ced57a1ce2fe5
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-12-15 21:20:32 -0500

    server returns array of score differences

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,12 +10,37 @@ module.exports = function(app){
     app.post('/api/classes',function(req,res){
         console.log(req.body);
         const userData = req.body.form;
+        console.log(userData);
         res.send(queryClasses(userData));
     });
 
     function queryClasses(data){
         console.log(`Querying Classes...`);
         console.log(classes);
-        console.log(`Input: ${data}`);
+        let name = data.shift(); // throw away name
+        console.log(name);
+        let numData = data.map(num=>Number(num));
+        console.log(`Input: ${typeof numData[0]}`);
+        //  function to loop through all app data
+        const scoreDifferences = classes.data.map(item=>{
+            // loop through each class's score array
+            console.log(`current item`);
+            console.log(item);
+
+            // use reduce to take the scores array and reduce to single difference value for each class
+            // this is done by subtracting the current array value by the numData[reduce index], since both arrays are
+            // the same length
+            const difference = item.scores.reduce((sum,score,index)=>{
+                console.log(`Calculating ${score} - ${numData[index]}`);
+                sum += Math.abs(score-numData[index]);
+                // console.log(sum);
+                return sum;
+            },0);
+
+            //return the difference of each array compared to user's input
+            console.log(difference);
+            return difference;
+        });
+        console.log(scoreDifferences);
     }
 };
