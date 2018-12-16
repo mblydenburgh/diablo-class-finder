89a915efb8033f857b3ae8479c930f00bce94723
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-12-16 09:37:12 -0500

    added img url to class data, survey page displays simple html for results

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,6 @@ module.exports = function(app){
     });
 
     app.post('/api/classes',function(req,res){
-        console.log(req.body);
         const userData = req.body.form;
         console.log(userData);
         // console.log(queryClasses(userData));
@@ -19,34 +18,33 @@ module.exports = function(app){
         console.log(`Querying Classes...`);
         console.log(classes);
         let name = data.shift(); // throw away name
-        console.log(name);
+        // console.log(name);
+        //convert received form data from array of strings to array of numbers
         let numData = data.map(num=>Number(num));
-        console.log(`Input: ${typeof numData[0]}`);
-        //  function to loop through all app data
+
+        //  map through all app data to find score differences, store them in an array
         const scoreDifferences = classes.data.map(item=>{
             // loop through each class's score array
-            console.log(`current item`);
-            console.log(item);
+            // console.log(`current item`);
+            // console.log(item);
 
             // use reduce to take the scores array and reduce to single difference value for each class
             // this is done by subtracting the current array value by the [reduce index], since both arrays are
             // the same length
             const difference = item.scores.reduce((sum,score,index)=>{
-                console.log(`Calculating ${score} - ${numData[index]}`);
+                // console.log(`Calculating ${score} - ${numData[index]}`);
                 sum += Math.abs(score-numData[index]);
-                // console.log(sum);
                 return sum;
             },0);
 
             //return the difference of each array compared to user's input
-            console.log(difference);
             return difference;
         });
         console.log(scoreDifferences);
+        // find the minimum value in the differences array
         const lowestDifference = Math.min(...scoreDifferences);
-        // console.log(`Lowest: ${lowestDifference}`);
+        // get the index of the lowest value
         const matchingIndex = scoreDifferences.indexOf(lowestDifference);
-        // console.log(matchingIndex);
         console.log(`Sending match: ${classes.data[matchingIndex].name}`)
         const result = classes.data[matchingIndex];
         return result;
